23270458cfd7729179c0e527f3aa7b5789ca9aec
toliahoang <tung.istu@gmail.com>
2020-08-17 21:37:36 +0700

    Create Summary_boxplot.py

---
+++ Summary_boxplot.py
@@ -0,0 +1,6 @@
+The box of a boxplot visualizes the median, first quartile, and third quartile of a dataset.
+The length of the box in a boxplot visualizes the interquartile range.
+The whiskers extend from the box 1.5 times the size of the interquartile range.
+Outliers are points that fall outside of the whiskers. They’re represented by dots.
+Boxplots are especially useful for comparing the spread of multiple datasets.
+
